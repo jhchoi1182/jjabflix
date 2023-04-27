82d2cf383c8294eb59b63b43607592a57f08ba08
jhchoi1182 <jhchoi1182@gmail.com>
2023-04-27 23:19:47 +0900

    ✨체크 아이콘 추가

--- src/components/atoms/Icons/ButtonIcon.tsx
+++ src/components/atoms/Icons/ButtonIcon.tsx
@@ -51,6 +51,24 @@ export const AddIcon = ({ size }: SizeProps) => {
   );
 };
 
+export const CheckIcon = ({ size }: SizeProps) => {
+  return (
+    <svg
+      width={`${size}vw`}
+      height={`${size}vw`}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Checkmark"
+    >
+      <path
+        d="M8.68239 19.7312L23.6824 5.73115L22.3178 4.26904L8.02404 17.6098L2.70718 12.293L1.29297 13.7072L7.29297 19.7072C7.67401 20.0882 8.28845 20.0988 8.68239 19.7312Z"
+        fill="currentColor"
+      ></path>
+    </svg>
+  );
+};
+
 export const ContentOpenIcon = ({ size }: SizeProps) => {
   return (
     <svg
