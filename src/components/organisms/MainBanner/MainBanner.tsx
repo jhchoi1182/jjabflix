f2d9234fcbc7ae032560101fc759ddd51d288699
jhchoi1182 <jhchoi1182@gmail.com>
2023-05-02 15:21:20 +0900

    ♻️불필요한 패턴 삭제

--- src/components/organisms/MainBanner/MainBanner.tsx
+++ src/components/organisms/MainBanner/MainBanner.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import { detailAPI, posterAPI } from "../../../api/Apis";
 import { useQuery } from "@tanstack/react-query";
 import { IResult } from "../../../interface/Interface";
-import { ChildrenProps } from "../../../utils/type";
 import * as Button from "../../molecules/Button/RectangleButton";
 import * as fonts from "../../../styles/Fonts";
 
@@ -11,19 +10,14 @@ export type MainBannerProps = {
   media_type: string;
 };
 
-type BannerContentProps = {
-  Title: React.FC<ChildrenProps>;
-  Detail: React.FC<ChildrenProps>;
-};
-
-const MainBanner: React.FC<MainBannerProps> & BannerContentProps = ({ id, media_type }) => {
+const MainBanner: React.FC<MainBannerProps> = ({ id, media_type }) => {
   const { data } = useQuery<IResult | undefined>(["bannerDetail"], () => detailAPI({ id, media_type }));
   const { backdrop_path, poster_path, title, name, overview } = data || {};
 
   return (
     <Banner bgimg={posterAPI(backdrop_path ?? poster_path)}>
-      <MainBanner.Title>{title ?? name}</MainBanner.Title>
-      <MainBanner.Detail>{overview}</MainBanner.Detail>
+      <Title>{title ?? name}</Title>
+      <Overview>{overview}</Overview>
       <ButtonBox>
         <Button.Play />
         <Button.Detaile />
@@ -45,13 +39,13 @@ const Banner = styled.div<{ bgimg: string }>`
   padding: 60px;
 `;
 
-MainBanner.Title = styled.h2`
+const Title = styled.h2`
   ${fonts.big}
   ${fonts.bold}
   margin-bottom: 2.3rem;
 `;
 
-MainBanner.Detail = styled.p`
+const Overview = styled.p`
   ${fonts.mid2}
   ${fonts.bold}
   width: 37%;
