aa5981e28cfed10cd0ff9c4b1f421824c0e3b01c
jhchoi1182 <jhchoi1182@gmail.com>
2023-05-07 18:35:23 +0900

    ✨푸터에 들어갈 아이콘들 추가

--- src/components/atoms/Icons/index.tsx
+++ src/components/atoms/Icons/index.tsx
@@ -1,6 +1,6 @@
 export { default as Arrow } from "./NextPrevIcon";
 export { PlayIcon, DetailIcon, AddIcon, ContentOpenIcon } from "./ButtonIcon";
-export { Logo, AdultIcon } from "./UIIcon";
+export { Logo, AdultIcon, FacebookIcon, InstagramIcon, TwitterIcon, YoutubeIcon } from "./UIIcon";
 
 export type SizeProps = {
   size: number;
