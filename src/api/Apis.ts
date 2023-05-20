4102c112f6cee643996a5452337abe14e3f43251
jhchoi1182 <jhchoi1182@gmail.com>
2023-05-20 21:52:36 +0900

    ♻️apis 리팩토링

--- src/api/Apis.ts
+++ src/api/Apis.ts
@@ -1,51 +1,52 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const API = process.env.REACT_APP_API;
+const LANGUAGE = "ko";
+const PAGE = "1";
+
+type Params = {
+  [key: string]: string | number | undefined;
+};
+
+const fetchData = async (url: string, params: Params) => {
+  const queryString = Object.entries(params)
+    .map(([key, value]) => value && `${key}=${encodeURIComponent(value)}`)
+    .join("&");
+  const response = await fetch(`${url}?${queryString}`);
+  return response.json();
+};
 
 export const movieAPI = {
-  nowPlaying: () => {
-    return fetch(`${BASE_URL}/movie/now_playing?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  popular: () => {
-    return fetch(`${BASE_URL}/movie/popular?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  top_rated: () => {
-    return fetch(`${BASE_URL}/movie/top_rated?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  upcoming: () => {
-    return fetch(`${BASE_URL}/movie/upcoming?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
+  nowPlaying: () => fetchData(`${BASE_URL}/movie/now_playing`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  popular: () => fetchData(`${BASE_URL}/movie/popular`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  top_rated: () => fetchData(`${BASE_URL}/movie/top_rated`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  upcoming: () => fetchData(`${BASE_URL}/movie/upcoming`, { api_key: API, language: LANGUAGE, page: PAGE }),
 };
 
 export const tvAPI = {
-  airing_today: () => {
-    return fetch(`${BASE_URL}/tv/airing_today?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  on_the_air: () => {
-    return fetch(`
-    ${BASE_URL}/tv/on_the_air?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  popular: () => {
-    return fetch(`${BASE_URL}/tv/popular?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
-  top_rated: () => {
-    return fetch(`${BASE_URL}/tv/top_rated?api_key=${API}&language=ko&page=1`).then((response) => response.json());
-  },
+  airing_today: () => fetchData(`${BASE_URL}/tv/airing_today`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  on_the_air: () => fetchData(`${BASE_URL}/tv/on_the_air`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  popular: () => fetchData(`${BASE_URL}/tv/popular`, { api_key: API, language: LANGUAGE, page: PAGE }),
+  top_rated: () => fetchData(`${BASE_URL}/tv/top_rated`, { api_key: API, language: LANGUAGE, page: PAGE }),
 };
 
 export const homeAPI = {
-  trending: () => {
-    return fetch(`${BASE_URL}/trending/all/day?api_key=${API}&language=ko`).then((response) => response.json());
-  },
+  trending: () => fetchData(`${BASE_URL}/trending/all/day`, { api_key: API, language: LANGUAGE }),
 };
 
 export const searchAPI = (keyword: string) => {
-  return fetch(`${BASE_URL}/search/multi?api_key=${API}&language=ko&query=${keyword}&page=1&include_adult=true`).then(
-    (response) => response.json()
-  );
+  return fetchData(`${BASE_URL}/search/multi`, {
+    api_key: API,
+    language: LANGUAGE,
+    query: keyword,
+    page: PAGE,
+    include_adult: "true",
+  });
 };
 
 export const posterAPI = (path?: string, size?: string) => {
-  if (!path) return `https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930`;
+  if (!path) {
+    return "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930";
+  }
   return `https://image.tmdb.org/t/p/${size ?? "original"}/${path}`;
 };
 
@@ -56,9 +57,10 @@ type detailAPIParameter = {
 
 export const detailAPI = ({ id, media_type }: detailAPIParameter) => {
   if (id === 0) return;
-  if (media_type === "movie")
-    return fetch(`${BASE_URL}/movie/${id}?api_key=${API}&language=ko`).then((response) => response.json());
-  if (media_type === "tv")
-    return fetch(`${BASE_URL}/tv/${id}?api_key=${API}&language=ko`).then((response) => response.json());
-  else return;
+  if (media_type === "movie") {
+    return fetchData(`${BASE_URL}/movie/${id}`, { api_key: API, language: LANGUAGE });
+  }
+  if (media_type === "tv") {
+    return fetchData(`${BASE_URL}/tv/${id}`, { api_key: API, language: LANGUAGE });
+  }
 };
